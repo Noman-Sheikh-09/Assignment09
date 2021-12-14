3532abccb9d612b6ff256ee13fbe4c7943948f44
noman <nomanshk310@gmail.com>
2021-12-15 00:48:19 +0500

    Programs of Arrays

---
+++ script.js
@@ -0,0 +1,28 @@
+// 1. Write a js program to read and print elements of array.
+
+// var numbers = [1,2,3,4,5,6,7,8,9]
+// console.log("Total Element", numbers);
+
+// 2. Write a js program to print all negative elements in an array.
+
+// var numbers = [1,2,-3,4,-5,-6,7,8,9,10,12,11,34,65,-45,];
+// var neg = numbers.filter(negativeNumbers);
+// function negativeNumbers(neg)
+// {
+//     return neg < 0 ;
+// }
+// console.log("Negative Number is",neg);
+
+// 3. Write a js program to find sum of all array elements. 
+
+// var numbers = [1,2,3,4,5,6,7,8,9]
+// let sum = 0;
+// numbers.forEach(sumAll)
+// function sumAll(item){
+//     sum += item;
+//    console.log(sum);
+// }
+
+
+// 4. Write a js program to find maximum and minimum element in an array.
+// var numbers = [10,2,3,14,15,6,37,8,9]
